1d3bd2a577c26a1c47ebc21bffb71b0f1c34a892
Kamran Aliyev <keliyev2@std.beu.edu.az>
2025-04-29 16:13:50 +0400

    without / 2

--- src/main/java/com/example/simple_ecommerce/user/UserServiceApplication.java
+++ src/main/java/com/example/simple_ecommerce/user/UserServiceApplication.java
@@ -3,7 +3,7 @@ package com.example.simple_ecommerce.user;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "com.example.simple_ecommerce.user")
+@SpringBootApplication(scanBasePackages = "com.example.simple_ecommerce")
 
 public class UserServiceApplication {
     public static void main(String[] args) {
